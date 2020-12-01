17bac67ba74b0dbc9c5466631f27e40d90f2ee91
Carlo Butelli <carlo@digifit.eu>
2020-12-01 22:26:38 +0100

    added footer

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../heroSection/HeroSection';
 import Cards from "../cards/Cards";
+import Footer from "../footer/Footer";
 
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
+      <Footer/>
     </>
   )
 }
